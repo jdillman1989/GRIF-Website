a8030825fc159f5693f6356b0e07e51f94ea87b2
Jesse Dillman <jesse@thinkboxcreative.com>
2018-01-10 22:18:52 -0500

    init

---
+++ app/custom-fields/front-page.acf.php
@@ -0,0 +1,19 @@
+<?php
+core_register_field_group( array(
+	'title' => 'Home Options',
+	'location' => array(
+		array(
+			array(
+				'param' => 'page_type',
+				'operator' => '==',
+				'value' => 'front_page'
+			)
+		)
+	),
+	'hide_on_screen' => array( 'the_content' ),
+	'fields' => array(
+
+		core_field( 'text', 'Hello' )
+
+	)
+) );
---
+++ app/custom-fields/options.acf.php
@@ -0,0 +1,18 @@
+<?php
+
+core_register_field_group( array(
+        'title' => 'Site Options',
+        'location' => array(
+            array(
+                array(
+                    'param' => 'options_page',
+                    'operator' => '==',
+                    'value' => 'acf-options'
+                )
+            )
+        ),
+        'fields' => array(
+
+        )
+    )
+);
