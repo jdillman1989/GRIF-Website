b7286d7406347c1a65b585fd70520a3b18b04177
Jesse Dillman <jesse@thinkboxcreative.com>
2018-07-11 13:53:45 -0400

    archive template

--- functions.php
+++ functions.php
@@ -288,9 +288,7 @@ class Default_Page extends Core_Template {
 		$fields = get_field('builder_blocks', $this->obj_id);
 		$return = array();
 
-		var_dump(count($fields));
-
-		if (count($fields)) {
+		if (count($fields) > 1) {
 			foreach ($fields as $field) {
 				switch($field['acf_fc_layout']){
 					case 'site_cta':
